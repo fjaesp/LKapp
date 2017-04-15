7d1ec880628ac5264f070eb5919ce305a1c7441a
Espen Fjalestad <fjalestad@gmail.com>
2017-04-15 17:08:47 +0200

    Mottak av push-notifications fungerer

--- LK/LK.iOS/AppDelegate.cs
+++ LK/LK.iOS/AppDelegate.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-
+using Newtonsoft.Json.Linq;
 using Foundation;
 using UIKit; using Microsoft.Identity.Client;
+using Microsoft.WindowsAzure.MobileServices;
 
 namespace LK.iOS
 {
@@ -23,7 +24,46 @@ namespace LK.iOS
 			var platformParameters = UIApplication.SharedApplication.KeyWindow.RootViewController;
 			App.AuthenticationClient.PlatformParameters = new PlatformParameters(platformParameters);
 
+			// Register for push notifications.
+			var settings = UIUserNotificationSettings.GetSettingsForTypes(
+				UIUserNotificationType.Alert
+				| UIUserNotificationType.Badge
+				| UIUserNotificationType.Sound,
+				new NSSet());
+
+			UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+			UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
 			return result;
         }
+		public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+		{
+			const string templateBodyAPNS = "{\"aps\":{\"alert\":\"$(messageParam)\"}}";
+
+			JObject templates = new JObject();
+			templates["genericMessage"] = new JObject
+				 {
+				   {"body", templateBodyAPNS}
+				 };
+
+			// Register for push with your mobile app
+			Push push = Managers.NotificationManager.DefaultManager.CurrentClient.GetPush();
+			push.RegisterAsync(deviceToken, templates);
+		}
+		public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+		{
+			NSDictionary aps = userInfo.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+			string alert = string.Empty;
+			if (aps.ContainsKey(new NSString("alert")))
+				alert = (aps[new NSString("alert")] as NSString).ToString();
+
+			//show alert
+			if (!string.IsNullOrEmpty(alert))
+			{
+				UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+				avAlert.Show();
+			}
+		 }
     }
 }
