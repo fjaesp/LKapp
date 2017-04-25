a7911302ce6ad134cd627afa4cabb0d1bebc925a
JK <johnkennethhelland@gmail.com>
2017-04-25 21:24:35 +0200

    Småjusteringer event

--- LK/LK/ViewModels/CustomWebView.cs
+++ LK/LK/ViewModels/CustomWebView.cs
@@ -9,12 +9,14 @@ namespace LK.ViewModels
 {
     public class CustomWebView : WebView
     {
-        public static readonly BindableProperty UriProperty = BindableProperty.Create<CustomWebView, string>(p => p.Uri, default(string));
+        //private static readonly BindableProperty uriProperty = BindableProperty.Create<CustomWebView, string>(p => p.Uri, default(string));
 
-        public string Uri
-        {
-            get { return (string)GetValue(UriProperty); }
-            set { SetValue(UriProperty, value); }
-        }
+        //public static BindableProperty UriProperty => uriProperty;
+
+        //public string Uri
+        //{
+        //    get { return (string)GetValue(UriProperty); }
+        //    set { SetValue(UriProperty, value); }
+        //}
     }
 }
